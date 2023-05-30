f74ae40e2f89b2cf744f709a2af2cdba055b9874
preksha <jainpreksha2712@gmail.com>
2023-05-30 18:28:28 +0530

    find all indexes of x using recursion

--- Recursion/AllIndexes.cpp
+++ Recursion/AllIndexes.cpp
@@ -1,6 +1,38 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+int IndexCount(int *arr , int *output, int size, int x){
+    if(size == 0){
+        return 0;
+    }
+    int short_ans = IndexCount(arr+1, output, size-1, x);
 
+    for (int i=0;i<short_ans;i++){
+        output[i] = output[i]+1;
+    }
+    if(arr[0]== x){
+        for(int i=short_ans-1;i>=0;i--){
+            output[i+1] = output[i]; 
+        }
+        output[0] = 0;
+        return short_ans+1;
+    }
+    else{
+        return short_ans;
+    }
+}
+
+void IndexArray(int *arr, int size,int x){
+    int output[size];
+    int len = IndexCount(arr,output,size,x);
+    for(int i=0;i<len;i++){
+        cout << output[i]<< " ";
+    }
+}
+
+int main(){
+    int arr[] = {1,2,3,4,1,7};
+    cout<< "Output: ";
+    IndexArray(arr,7,1);
+    return 0;
 }
