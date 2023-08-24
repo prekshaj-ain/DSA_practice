4e4e7b1238ac3ad880cb463ef9b290728e465f63
preksha <jainpreksha2712@gmail.com>
2023-08-24 18:17:47 +0530

    insert duplicates in binary tree

---
+++ binary_trees/insertDuplicates.cpp
@@ -0,0 +1,92 @@
+// Create  Insert Duplicate Node
+#include <iostream>
+#include "BinaryTree.h"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+BinaryTreeNode<int> *levelOrderInput()
+{
+    int rootData;
+    queue<BinaryTreeNode<int> *> q;
+    cout << "Enter data";
+    cin >> rootData;
+    if (rootData == -1)
+        return NULL;
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    q.push(root);
+    while (!q.empty())
+    {
+        BinaryTreeNode<int> *front = q.front();
+        q.pop();
+        cout << "Enter left child of " << front->data;
+        int left;
+        cin >> left;
+        if (left != -1)
+        {
+            BinaryTreeNode<int> *leftNode = new BinaryTreeNode<int>(left);
+            front->left = leftNode;
+            q.push(leftNode);
+        }
+        cout << "Enter right child of " << front->data;
+        int right;
+        cin >> right;
+        if (right != -1)
+        {
+            BinaryTreeNode<int> *rightNode = new BinaryTreeNode<int>(right);
+            front->right = rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+BinaryTreeNode<int> *insertDuplicates(BinaryTreeNode<int> *root)
+{
+    if (root == NULL)
+        return NULL;
+    if (root->left)
+    {
+        root->left = insertDuplicates(root->left);
+    }
+    if (root->right)
+    {
+        root->right = insertDuplicates(root->right);
+    }
+    BinaryTreeNode<int> *dupNode = new BinaryTreeNode<int>(root->data);
+    dupNode->left = root->left;
+    root->left = dupNode;
+    return root;
+}
+void printLevelOrder(BinaryTreeNode<int> *root)
+{
+    if (root == NULL)
+        return;
+    cout << root->data << " ";
+    queue<BinaryTreeNode<int> *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        BinaryTreeNode<int> *front = q.front();
+        q.pop();
+        if (front->left)
+        {
+            cout << front->left->data << " ";
+            q.push(front->left);
+        }
+        if (front->right)
+        {
+            cout << front->right->data << " ";
+            q.push(front->right);
+        }
+    }
+}
+
+int main()
+{
+    BinaryTreeNode<int> *root = levelOrderInput();
+    root = insertDuplicates(root);
+    cout << endl;
+    printLevelOrder(root);
+    return 0;
+}
